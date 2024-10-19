72f3fe94d134fab1fb6e2f22f0632405f33622af
Prthmsh7 <Prathmeshshukla2106@gmail.com>
2024-10-19 15:18:11 +0530

    updated the code

---
+++ Frontend/src/components/Footer.js
@@ -0,0 +1,125 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
+import newsletter from "../images/newsletter.png";
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src={newsletter} alt="newsletter" />
+                <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Your Email Address"
+                  aria-label="Your Email Address"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                Jaypee University of Engineering and Technology, A-B Road, Raghogarh, Distt. - Guna M.P., INDIA <br />
+                  PinCode: 473226
+                </address>
+                <a
+                  href="tel:+91 8264954234"
+                  className="mt-3 d-block mb-1 text-white"
+                >
+                  +91 1111111111
+                </a>
+                <a
+                  href="mailto:devjariwala8444@gmail.com"
+                  className="mt-2 d-block mb-0 text-white"
+                >
+                  221b275@juetguna.in
+                </a>
+                <div className="social_icons d-flex align-items-center gap-30 mt-4">
+                  <a className="text-white" href="#">
+                    <BsLinkedin className="fs-4" />
+                  </a>
+                  <a className="text-white" href="#">
+                    <BsInstagram className="fs-4" />
+                  </a>
+                  <a className="text-white" href="#">
+                    <BsGithub className="fs-4" />
+                  </a>
+                  <a className="text-white" href="#">
+                    <BsYoutube className="fs-4" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link to="/privacy-policy" className="text-white py-2 mb-1">
+                  Privacy Policy
+                </Link>
+                <Link to="/refund-policy" className="text-white py-2 mb-1">
+                  Refund Policy
+                </Link>
+                <Link to="/shipping-policy" className="text-white py-2 mb-1">
+                  Shipping Policy
+                </Link>
+                <Link to="/term-conditions" className="text-white py-2 mb-1">
+                  Terms & Conditions
+                </Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Tools</Link>
+                <Link className="text-white py-2 mb-1">Seeds</Link>
+                <Link className="text-white py-2 mb-1">Supplies</Link>
+                <Link className="text-white py-2 mb-1">Pesticides</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}; Powered by Farmfolio
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
