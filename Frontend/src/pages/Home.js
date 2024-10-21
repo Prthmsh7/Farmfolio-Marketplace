f8a8670e26d9b15fdce86e80278470f4e80343c8
dhruvS-py <dhruv123singh321@gmail.com>
2024-10-21 14:49:11 +0530

    updated the brand images

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -464,28 +464,28 @@ const Home = () => {
             <div className="marquee-inner-wrapper card-wrapper">
               <Marquee className="d-flex">
                 <div className="mx-4 w-25">
-                  <img src="images/brand-01.png" alt="brand" />
+                  <img src="images/Mahindra-Logo-2012-1-600x338.webp" alt="brand" />
                 </div>
                 <div className="mx-4 w-25">
-                  <img src="images/brand-02.png" alt="brand" />
+                  <img src="images/Mahindra-Logo-2012-1-600x338.webp" alt="brand" />
                 </div>
                 <div className="mx-4 w-25">
-                  <img src="images/brand-03.png" alt="brand" />
+                  <img src="images/Mahindra-Logo-2012-1-600x338.webp" alt="brand" />
                 </div>
                 <div className="mx-4 w-25">
-                  <img src="images/brand-04.png" alt="brand" />
+                  <img src="images/Mahindra-Logo-2012-1-600x338.webp" alt="brand" />
                 </div>
                 <div className="mx-4 w-25">
-                  <img src="images/brand-05.png" alt="brand" />
+                  <img src="images/Mahindra-Logo-2012-1-600x338.webp" alt="brand" />
                 </div>
                 <div className="mx-4 w-25">
-                  <img src="images/brand-06.png" alt="brand" />
+                  <img src="images/Mahindra-Logo-2012-1-600x338.webp" alt="brand" />
                 </div>
                 <div className="mx-4 w-25">
-                  <img src="images/brand-07.png" alt="brand" />
+                  <img src="images/Mahindra-Logo-2012-1-600x338.webp" alt="brand" />
                 </div>
                 <div className="mx-4 w-25">
-                  <img src="images/brand-08.png" alt="brand" />
+                  <img src="images/Mahindra-Logo-2012-1-600x338.webp" alt="brand" />
                 </div>
               </Marquee>
             </div>
